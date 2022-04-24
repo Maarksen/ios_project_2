bc32e1dbe5ec36f54098d6f26b4057d10c1a9d7d
Maarksen <buch.mamo@gmail.com>
2022-04-24 22:50:41 +0200

    works without semaphores TODO semaphores

--- proj2.c
+++ proj2.c
@@ -6,9 +6,20 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <fcntl.h>
 
-#define NUM_ARG 5 //number of intended arguments
-#define MAX_TIME 1000 //maximum number that time can be set to
+#define NUM_ARG 5       //number of intended arguments
+#define MAX_TIME 1000   //maximum number that time can be set to
+
+//allocating shared memory
+#define MMAP(pointer){(pointer) = mmap(NULL, sizeof(*(pointer)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);}
+#define UNMAP(pointer) {munmap((pointer), sizeof((pointer)));}
 
 //prototypes of functions to verify arguments
 bool enough_arguments(int argc);
@@ -18,11 +29,24 @@ bool check_time(int time);
 //prototype of a function to put a process to sleep
 void rand_sleep(unsigned time);
 
-//prototypes of functions creating molecules
-int oxygen(int id_o, unsigned wait_time);
-int hydrogen(int id_h, unsigned wait_time);
-void create_molecule(sem_t oxygen, sem_t hydrogen, unsigned num_hydrogen, unsigned num_oxygen);
+//prototypes of functions for initialization
+void init();
+void destruct();
 
+//prototypes of functions creating molecules
+void oxygen(int id_o, unsigned wait_time, unsigned create_time);
+void hydrogen(int id_h, unsigned wait_time, unsigned create_time);
+void create_molecule(unsigned create_time);
+
+//declarations of shared variables
+int *line = NULL;
+int *num_hydrogen = NULL;
+int *num_oxygen = NULL;
+int *id_m = NULL;
+
+//declarations of semaphores
+sem_t oxy;
+sem_t hydro;
 
 int main(int argc, char *argv[]){
 
@@ -40,12 +64,6 @@ int main(int argc, char *argv[]){
     unsigned wait_time = atoi(argv[3]);
     unsigned create_time = atoi(argv[4]);
 
-    sem_t oxy;
-    sem_t hydro;
-
-    unsigned num_hydrogen;
-    unsigned num_oxygen;
-
     if(!(check_time(wait_time))){
         return 1;
     }
@@ -53,15 +71,20 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    init();
+
+    (*num_oxygen) = 0;
+    (*num_hydrogen) = 0;
+
     pid_t pid;
 
     for(int i = 0; i < number_oxygen; i++){
         pid = fork();
         if(pid == -1){
-            printf(stderr,"error forking oxygen");
+            fprintf(stderr,"error forking oxygen");
         }
         if(pid == 0){
-            oxygen(i+1, wait_time);
+            oxygen(i+1, wait_time, create_time);
             exit(0);
         }
     }
@@ -69,14 +92,15 @@ int main(int argc, char *argv[]){
     for(int i = 0; i < number_hydrogen; i++){
         pid = fork();
         if(pid == -1){
-            printf(stderr,"error forking hydrogen");
+            fprintf(stderr,"error forking hydrogen");
         }
         if(pid == 0){
-            hydrogen(i+1, wait_time);
+            hydrogen(i+1, wait_time, create_time);
             exit(0);
         }
     }
 
+    destruct();
     return 0;
 }
 
@@ -113,31 +137,60 @@ bool check_time(int time){
     return true;
 }
 
-//function creating oxygen
-int oxygen(int id_o, unsigned wait_time){
-    printf("O %d: started\n", id_o);
-    rand_sleep(wait_time);
-    printf("O %d: going to queue\n", id_o);
+//initializing shared memory and semaphores
+void init(){
+    MMAP(line);
+    MMAP(num_oxygen);
+    MMAP(num_hydrogen);
+    MMAP(id_m)
+}
 
-    return 0;
+//destroying shared memory and semaphores
+void destruct(){
+    UNMAP(line);
+    UNMAP(num_oxygen);
+    UNMAP(num_hydrogen);
+    MMAP(id_m);
 }
 
 //function creating oxygen
-int hydrogen(int id_h, unsigned wait_time){
-    printf("H %d: started\n", id_h);
+void oxygen(int id_o, unsigned wait_time, unsigned create_time){
+    printf("%d: O %d: started\n", ++(*line),  id_o);
+    ++(*num_oxygen);
     rand_sleep(wait_time);
-    printf("H %d: going to queue\n", id_h);
+    printf("%d: O %d: going to queue\n", ++(*line), id_o);
 
-    return 0;
+    create_molecule(create_time);
+}
+
+//function creating oxygen
+void hydrogen(int id_h, unsigned wait_time, unsigned create_time){
+    printf("%d: H %d: started\n", ++(*line), id_h);
+    ++(*num_hydrogen);
+    rand_sleep(wait_time);
+    printf("%d: H %d: going to queue\n", ++(*line), id_h);
+
+    create_molecule(create_time);
 }
 
 //function creating individual molecules
-void create_molecule(sem_t oxy, sem_t hydro, unsigned num_hydrogen, unsigned num_oxygen){
-    sem_post(&(hydro));
+void create_molecule(unsigned create_time){
+    if((*num_oxygen) >= 1 && (*num_hydrogen) >= 2){
+        ++(*id_m);
+        int index = (*id_m);
+        printf("%d: Creating molecule %d\n",++(*line), index);
+        (*num_oxygen) -= 1;
+        (*num_hydrogen) -= 2;
+
+        rand_sleep(create_time);
+        printf("%d: Molecule %d created\n",++(*line), index);
+    }
+
+    /*sem_post(&(hydro));
     sem_post(&(hydro));
     num_hydrogen -= 2;
     sem_post(&(oxy));
-    num_oxygen -= 1;
+    num_oxygen -= 1;*/
 }
 
 //function that puts a process to sleep
